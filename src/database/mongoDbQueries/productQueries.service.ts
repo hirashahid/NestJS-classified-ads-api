fe6dc4a0f02acc71ec94a798d084cb550ac2c95e
Hira Shahid <itshira1412@yahoo.com>
2023-10-24 14:27:06 +0500

    Throwing db error

--- src/database/mongoDbQueries/productQueries.service.ts
+++ src/database/mongoDbQueries/productQueries.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { MongoPrismaService } from '@app/database/mongo-prisma.service';
-import { ErrorWhileCreating } from '@app/exceptions/custom.exception';
-import { errorMessages } from '@app/common/constants/errorMessages';
 
 @Injectable()
 export class ProductQueriesService {
   constructor(private readonly mongoPrisma: MongoPrismaService) {}
 
   async createProduct(model: string, data: any) {
-    const product = await this.mongoPrisma[model].create({
-      data,
-    });
-    if (!product)
-      throw new ErrorWhileCreating(errorMessages.errorWhileCreating);
-    return product;
+    try {
+      const product = await this.mongoPrisma[model].create({
+        data,
+      });
+      return product;
+    } catch (error) {
+      throw new Error(`Database error: ${error.message}`);
+    }
   }
 }
